e834fd8dfb8ea5024a7c2d56edf54678eaa4075b
Ayomide Oshilaja <137465548+Amy23m@users.noreply.github.com>
2024-12-06 22:15:18 -0700

    Add files via upload

---
+++ midterm1.1/midterm1.js
@@ -0,0 +1,12 @@
+
+
+// Function to toggle between light and dark mode
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    const button = document.getElementById('theme-toggle');
+    
+    
+}
+
+
